e4102e915eb967e770051d47cd5d725009674b23
Kanika Wadhawan <wadhawan.kanika@gmail.com>
2019-09-06 16:07:04 -0700

    added button to the single events

--- themes/red_amp/archive-event.php
+++ themes/red_amp/archive-event.php
@@ -10,23 +10,24 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 	   
-		<div class="events-title">
-	<?php post_type_archive_title('<h1>','</h1>');?>
-</div>
-
-    <div class="events-description">
 	
-</div>
+    
+	
+
 	<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
-				<?php
-				the_archive_description( '<div class="taxonomy-description">', '</div>' );
-				?>
+			<div class="events-title">
+				<?php post_type_archive_title('<h1>','</h1>');?>
+			</div>
 
-             
-
-			</header><!-- .page-header -->
+			<div class="events-description">
+			<?php
+			echo CFS()->get('option_event_description', 134); // TODO change id to match options page on staging site
+			?>
+            </div>
+			
+            </header><!-- .page-header -->
 
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
@@ -34,7 +35,12 @@ get_header(); ?>
 			<?php echo	"<a href=".esc_url( get_permalink()). ">"; ?>
 				<?php
 				   
-				   echo date( 'jS F Y' ,strtotime(CFS()-> get('event_date')));
+				//    echo CFS()-> get('event_date', 127);
+				   $event_date = date( 'jS F Y' ,strtotime(CFS()-> get('event_date')));
+				//    var_dump( $event_date );
+				//    if(!empty($event_date)){
+					echo $event_date;
+				//    }
 					get_template_part( 'template-parts/content' );
 					
 				?>
---
+++ themes/red_amp/site-options.php
@@ -0,0 +1,8 @@
+<?php 
+/*
+ * Template Name: Site Options
+ */
+
+ echo 'this is an options page, not visible by the public';
+ die();
+?>
