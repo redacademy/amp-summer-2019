624102304162a2f949e3a482943f1d71700515ff
Matt Waraich <mattsinghwaraich@gmail.com>
2019-09-12 09:58:54 -0700

    About title removed

--- themes/red_amp/page-about.php
+++ themes/red_amp/page-about.php
@@ -10,7 +10,7 @@ get_header(); ?>
 	<div id="primary" class="content-area about-content">
 	<main id="main" class="site-main" role="main">
 		<?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
 
 	<div class="about-header-shadow">
 
