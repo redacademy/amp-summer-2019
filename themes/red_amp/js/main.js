daa98b79bc82f6aa0c02df6c4c7c5fb676549496
Alex Faissal <alefaissal@gmail.com>
2019-09-13 11:58:54 -0700

    Add back to top link in mobile, also JS and CSS for this link. All in front page

--- themes/red_amp/js/main.js
+++ themes/red_amp/js/main.js
@@ -72,19 +72,29 @@
 
     // end of function for book a tour button
 
-
-    $('.button-arrow').on('click', function (event) {
-      event.preventDefault();
-      const $href = $(event.currentTarget).attr('data-scrollto'); // value = #goto
-      console.log($($href).offset().top);
-      console.log('test');
-      $('body,html').animate({
-        scrollTop: $($href).offset().top
-      });
-    });
   }
 
 
+
+  $('.button-arrow').on('click', function (event) {
+    event.preventDefault();
+    const $href = $(event.currentTarget).attr('data-scrollto'); // value = #goto
+    console.log($($href).offset().top);
+    console.log('test');
+    $('body,html').animate({
+      scrollTop: $($href).offset().top
+    });
+  });
+
+  $('.mobile-back-to-top').on('click', function (event) {
+    event.preventDefault();
+    const $href = $(event.currentTarget).attr('data-scrollto'); // value = #goto
+    console.log($($href).offset().top);
+    console.log('test2');
+    $('body,html').animate({
+      scrollTop: $($href).offset().top
+    });
+  });
 
 
 })(jQuery);
